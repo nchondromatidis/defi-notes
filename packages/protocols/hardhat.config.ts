3fa555b13d28f0cf6f6631acf5a96c19ae850dd4
Nikos Chondromatidis <nchondromatidis@gmail.com>
2025-09-14 00:17:30 +0300

    protocols: removed ignition and viem plugin

--- packages/protocols/hardhat.config.ts
+++ packages/protocols/hardhat.config.ts
@@ -1,10 +1,8 @@
 import type { HardhatUserConfig } from "hardhat/config";
 
-import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
 import { configVariable } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  plugins: [hardhatToolboxViemPlugin],
   solidity: {
     compilers: [
       { version: "0.4.26" },
