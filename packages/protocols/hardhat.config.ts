3ac2689d25357ee677a3b51fd588ddf3455e0093
Nikos Chondromatidis <nchondromatidis@gmail.com>
2025-09-13 16:46:51 +0300

    protocols: added uniswap-v2 source code

---
+++ packages/protocols/hardhat.config.ts
@@ -0,0 +1,34 @@
+import type { HardhatUserConfig } from "hardhat/config";
+
+import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
+import { configVariable } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  plugins: [hardhatToolboxViemPlugin],
+  solidity: {
+    compilers: [
+      { version: "0.4.26" },
+      { version: "0.5.16" },
+      { version: "0.6.6" },
+      { version: "0.6.12" },
+    ],
+  },
+  networks: {
+    hardhatMainnet: {
+      type: "edr-simulated",
+      chainType: "l1",
+    },
+    hardhatOp: {
+      type: "edr-simulated",
+      chainType: "op",
+    },
+    sepolia: {
+      type: "http",
+      chainType: "l1",
+      url: configVariable("SEPOLIA_RPC_URL"),
+      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+    },
+  },
+};
+
+export default config;
