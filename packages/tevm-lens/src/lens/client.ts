3eafbb56ae5ea101683dbcd8015b6ff6d10c1cea
Nikos Chondromatidis <nchondromatidis@gmail.com>
2025-10-13 23:52:32 +0300

    made default client to be a testClient

--- packages/tevm-lens/src/lens/client.ts
+++ packages/tevm-lens/src/lens/client.ts
@@ -1,13 +1,14 @@
-import { createClient, createTevmTransport, tevmReady, type TevmTransport } from 'tevm';
-import type { Account, Client } from 'viem';
+import { createTevmTransport, tevmReady, type TevmTransport } from 'tevm';
+import { type Account, createTestClient, type TestClient } from 'viem';
 
-export async function buildClient(nodeAccount: Account): Promise<Client<TevmTransport>> {
+export async function buildClient(nodeAccount: Account): Promise<TestClient<'anvil', TevmTransport>> {
   const tevmTransport = createTevmTransport({
     miningConfig: { type: 'auto' },
   });
-  const client = createClient({
+  const client = createTestClient({
     account: nodeAccount,
     transport: tevmTransport,
+    mode: 'anvil',
   });
   await tevmReady(client);
 
