ba78400fd9e331db3ccc29622c2834e29af607bb
Nikos Chondromatidis <nchondromatidis@gmail.com>
2025-11-08 23:06:26 +0200

    having removed the analysis package, its content will be housed in the docs app

--- packages/analysis/eslint.config.js
+++
@@ -1,4 +0,0 @@
-import config from "@defi-notes/config/eslint.config";
-
-/** @type {import("eslint").Linter.Config} */
-export default config;
--- packages/analysis/prettier.config.js
+++
@@ -1,3 +0,0 @@
-import baseConfig from "@defi-notes/config/prettier.config";
-
-export default baseConfig;
