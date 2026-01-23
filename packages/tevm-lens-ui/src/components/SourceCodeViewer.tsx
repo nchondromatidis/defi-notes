ce29580cb0a85a5ab4ed911ff1158812c88ac39b
Nikos Chondromatidis <nchondromatidis@gmail.com>
2026-01-24 00:29:57 +0200

    tevm-lens-ui: added source code viewer component (2)

--- packages/tevm-lens-ui/src/components/SourceCodeViewer.tsx
+++ packages/tevm-lens-ui/src/components/SourceCodeViewer.tsx
@@ -1,24 +1,15 @@
 import CodeMirror from '@uiw/react-codemirror';
 import { solidity } from '@replit/codemirror-lang-solidity';
 import { oneDark } from '@codemirror/theme-one-dark';
-import { useMemo } from 'react';
 
 interface SourceCodeViewerProps {
   sourceCode?: string;
-  language?: 'solidity' | 'plaintext';
 }
 
 export function SourceCodeViewer({
   sourceCode = '// Select a function to view its source code',
-  language = 'solidity',
 }: SourceCodeViewerProps) {
-  const extensions = useMemo(() => {
-    const exts = [];
-    if (language === 'solidity') {
-      exts.push(solidity);
-    }
-    return exts;
-  }, [language]);
+  const extensions = [solidity];
 
   return (
     <div className="h-full w-full overflow-hidden">
@@ -38,3 +29,5 @@ export function SourceCodeViewer({
     </div>
   );
 }
+
+//https://github.com/brimdata/react-arborist/blob/main/modules/showcase/pages/gmail.tsx
