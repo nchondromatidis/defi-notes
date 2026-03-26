e18d282fc63c3e700f08b48712a34c63053c6935
Nikos Chondromatidis <nchondromatidis@gmail.com>
2026-03-26 04:22:24 +0200

    protocols: disabled optimizer when on most solidity versions

--- packages/protocols/hardhat.config.ts
+++ packages/protocols/hardhat.config.ts
@@ -1,13 +1,19 @@
 import type { HardhatUserConfig } from 'hardhat/config';
 import augmentArtifacts from '@defi-notes/evm-lens-indexer';
 
-const compilerVersions: string[] = ['0.4.26', '0.5.16', '0.6.6', '0.6.12'];
+const compilerVersions = [
+  { v: '0.4.26', opt: true },
+  { v: '0.5.16', opt: true },
+  { v: '0.6.6', opt: false },
+  { v: '0.6.12', opt: false },
+];
 
-function createCompilerSettings(version: string) {
+function createCompilerSettings(version: string, optimize = false) {
+  const optimizer = optimize ? { enabled: true, runs: 100 } : { enabled: false };
   return {
     version,
     settings: {
-      optimizer: { enabled: true, runs: 100 },
+      optimizer,
       outputSelection: {
         '*': {
           '*': ['abi', 'evm.bytecode', 'evm.deployedBytecode'],
@@ -20,7 +26,7 @@ function createCompilerSettings(version: string) {
 const config: HardhatUserConfig = {
   plugins: [augmentArtifacts],
   solidity: {
-    compilers: compilerVersions.map(createCompilerSettings),
+    compilers: compilerVersions.map((it) => createCompilerSettings(it.v, it.opt)),
   },
   paths: {
     sources: './contracts',
